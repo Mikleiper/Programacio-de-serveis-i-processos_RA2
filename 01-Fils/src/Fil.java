e424b4c8bc5fb76cc5a93e141d6000263030d3ff
Mikleiper <mimuba55@gmail.com>
2025-12-11 19:51:12 +0100

    comportament1

--- 01-Fils/src/Fil.java
+++ 01-Fils/src/Fil.java
@@ -1,3 +1,27 @@
-public class Fil {
+public class Fil extends Thread {
+
+    private int comptador;
+    private long delay;
+    //constructor de la classe pare per assignar nom
+    public Fil(String nom, int comptador, long delay) {
+        // Llamamos al constructor de la clase padre Thread para asignar el nombre/id
+        // Teoría: "public Thread(String nom) -> rep el nom/id" [PDF pág 4]
+        super(nom);
+        this.comptador = comptador;
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= comptador; i++) {
+            
+            for (int j = 0; j < 1000; j++) {
+                // treball fictici
+            }
+            System.out.println(this.getName() + " " + i);
+
+        }
+        System.out.println("Acaba el fil " + this.getName());
+    }
 
 }
